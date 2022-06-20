3aa1d0feee576717259d876e61d97329b413b169
Denis Bezukladnikov <denis3094@mail.ru>
2022-06-20 16:00:47 +0700

    fix: Почистить код

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 const express = require('express');
+const { mongoose } = require('mongoose');
+const bodyParser = require('body-parser');
 
 const { PORT = 3000 } = process.env;
 const app = express();
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
+
 const usersRouter = require('./routes/users');
 const cardsRouter = require('./routes/cards');
 const { NOT_FOUND } = require('./constants/errors');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
