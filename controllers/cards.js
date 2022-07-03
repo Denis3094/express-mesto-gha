4442cf513e40519231e42c9462d167efe0d6eae1
Denis Bezukladnikov <idenis3094@yandex.ru>
2022-07-03 20:37:04 +0700

    fix: Исправить ошибку в контроллере удаления карточки

--- controllers/cards.js
+++ controllers/cards.js
@@ -28,7 +28,7 @@ module.exports.createCard = (req, res, next) => {
 };
 
 module.exports.deleteCard = (req, res, next) => {
-  Card.findByIdAndRemove(req.params.cardId)
+  Card.findById(req.params.cardId)
     .orFail(() => new NotFound('Карточка с указанным _id не найдена.'))
     .then((card) => {
       if (card.owner.toString() !== req.user._id) {
