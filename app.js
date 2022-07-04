6faa35e8598fa90d877f40cc05a126c29e29d7f4
Denis Bezukladnikov <idenis3094@yandex.ru>
2022-07-04 21:52:58 +0700

    fix: auth middleware

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
-require('dotenv').config();
 const express = require('express');
 const { mongoose } = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const { Joi, celebrate, errors } = require('celebrate');
-const usersRouter = require('./routes/users');
-const cardsRouter = require('./routes/cards');
-const { login, createUser } = require('./controllers/users');
-const auth = require('./middlewares/auth');
-const { handleError } = require('./middlewares/error');
-const { regExpLink } = require('./constants/regexp');
-const NotFound = require('./constants/NotFound');
+
+require('dotenv').config();
+
+const { PORT = 3000 } = process.env;
+const app = express();
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
@@ -18,8 +15,13 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   family: 4,
 });
 
-const { PORT = 3000 } = process.env;
-const app = express();
+const usersRouter = require('./routes/users');
+const cardsRouter = require('./routes/cards');
+const { login, createUser } = require('./controllers/users');
+const auth = require('./middlewares/auth');
+const { handleError } = require('./middlewares/error');
+const { regExpLink } = require('./constants/regexp');
+const NotFound = require('./constants/NotFound');
 
 app.use(cookieParser());
 app.use(bodyParser.json());
