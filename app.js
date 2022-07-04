c44eefe158075df4a901df90370355472e60157f
Denis Bezukladnikov <idenis3094@yandex.ru>
2022-07-04 22:24:47 +0700

    test: Autotest #14

--- app.js
+++ app.js
@@ -9,11 +9,7 @@ require('dotenv').config();
 const { PORT = 3000 } = process.env;
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/mestodb', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  family: 4,
-});
+mongoose.connect('mongodb://localhost:27017/mestodb');
 
 const usersRouter = require('./routes/users');
 const cardsRouter = require('./routes/cards');
