fe3fbd05002185a6c4456caf4fec44f3185239f9
Denis Bezukladnikov <idenis3094@yandex.ru>
2022-07-04 21:37:54 +0700

    test: Autotest #14

--- middlewares/error.js
+++ middlewares/error.js
@@ -4,4 +4,5 @@ module.exports.handleError = (err, req, res, next) => {
   res.status(statusCode).send({
     message: statusCode === 500 ? 'На сервере произошла ошибка!' : message,
   });
+  next();
 };
