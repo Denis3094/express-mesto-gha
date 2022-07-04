c1984b8dd20793c1176515e583bdcf66439972f2
Denis Bezukladnikov <idenis3094@yandex.ru>
2022-07-04 15:59:07 +0700

    feat: Добавить middleware ошибки, сделать небольшой рефакторинг кода

--- app.js
+++ app.js
@@ -4,19 +4,10 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const { Joi, celebrate, errors } = require('celebrate');
 
+require('dotenv').config();
+
 const { PORT = 3000 } = process.env;
 const app = express();
-
-const usersRouter = require('./routes/users');
-const cardsRouter = require('./routes/cards');
-const { login, createUser } = require('./controllers/users');
-const auth = require('./middlewares/auth');
-const { regExpLink, regExpEmail } = require('./constants/regexp');
-const NotFound = require('./constants/NotFound');
-
-app.use(cookieParser());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
@@ -24,9 +15,21 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   family: 4,
 });
 
+const usersRouter = require('./routes/users');
+const cardsRouter = require('./routes/cards');
+const { login, createUser } = require('./controllers/users');
+const auth = require('./middlewares/auth');
+const { handleError } = require('./middlewares/error');
+const { regExpLink } = require('./constants/regexp');
+const NotFound = require('./constants/NotFound');
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.post('/signin', celebrate({
   body: Joi.object().keys({
-    email: Joi.string().required().regex(regExpEmail),
+    email: Joi.string().required().email(),
     password: Joi.string().required().min(3),
   }),
 }), login);
@@ -35,7 +38,7 @@ app.post('/signup', celebrate({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
     avatar: Joi.string().regex(regExpLink),
-    email: Joi.string().required().regex(regExpEmail),
+    email: Joi.string().required().email(),
     password: Joi.string().required().min(3),
   }),
 }), createUser);
@@ -47,6 +50,7 @@ app.use((req, res, next) => {
   next(new NotFound('Страницы не существует!'));
 });
 app.use(errors({ message: 'Переданы некорректные данные!' }));
+app.use(handleError);
 
 app.listen(PORT, () => {
   console.log('app started', PORT);
